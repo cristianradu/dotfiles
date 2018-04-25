113558f994b184ef840f85fd1384de59e56888cd
Cristian Radu <code@cristianradu.com>
2018-04-24 22:14:44 -0700

    fixes for the init script

--- init.sh
+++ init.sh
@@ -10,14 +10,12 @@ done
 # add this script to .bashrc (if not already there)
 dotfilesInit="~/dotfiles/init.sh"
 
-if grep -q $dotfilesInit ~/.bashrc; then
-    exit 0
-fi
-
+if ! grep -q $dotfilesInit ~/.bashrc; then
 cat >> ~/.bashrc <<EOL
 
 # dotfiles
 # https://github.com/cristianradu/dotfiles
-$dotfilesInit
+[ -f $dotfilesInit ] && source $dotfilesInit
 
 EOL
+fi
