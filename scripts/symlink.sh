d3bca658a0935d1f9d32e121af73701715e08cc3
Cristian Radu <code@cristianradu.com>
2018-05-07 15:14:58 -0700

    fixes for the symlink script; added color output

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -4,22 +4,34 @@
 # https://github.com/paulmillr/dotfiles/blob/master/symlink-dotfiles.sh
 
 today=`date "+%Y%m%d"`
+# colors
+Reset='\033[0m'
+Yellow='\033[0;33m'       # Yellow
+BRed='\033[1;31m'         # Bright Red
+BGreen='\033[1;32m'       # Bright Green
+BYellow='\033[1;33m'      # Bright Yellow
+BBlue='\033[1;34m'        # Bright Blue
+
 dotfiles=~/dotfiles
 
 if [[ -d $dotfiles ]]; then
-  echo "Symlinking dotfiles from $dotfiles:"
+  echo -e "Symlinking dotfiles from ${BBlue}$dotfiles${Reset}";
 else
-  echo "$dotfiles does not exist!"
-  exit 1
+  echo -e "${BRed}$dotfiles${Reset} does not exist!";
+  exit 1;
 fi
 
 for dotfile in .{bash_profile,bashrc,editorconfig,gitconfig,gitignore};
 do
-  if [[ -f ~/$dotfile ]]; then
-    mv ~/$dotfile ~/${dotfile}.$today
-    echo "$dotfile already existed and was renamed to ${dotfile}.$today"
+  if [[ -h ~/$dotfile ]]; then
+    echo -e "${BGreen}$dotfile${Reset} is already symlinked";
+  else
+    if [[ -f ~/$dotfile ]]; then
+      mv ~/$dotfile ~/${dotfile}.$today
+      echo -e "${BYellow}$dotfile${Reset} already existed and was renamed to ${Yellow}${dotfile}.$today${Reset}";
+    fi
+    ln -s $dotfiles/$dotfile ~/$dotfile
   fi
-  ln -s $dotfiles/$dotfile ~/$dotfile
 done
 
 unset dotfile
